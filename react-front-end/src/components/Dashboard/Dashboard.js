833a95e7ff97db6a933f5a4907b580cc0ffde2f0
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-23 23:15:20 -0800

    Add legend table
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- react-front-end/src/components/Dashboard/Dashboard.js
+++ react-front-end/src/components/Dashboard/Dashboard.js
@@ -3,8 +3,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { Container } from '@material-ui/core/';
 import { PieChart } from 'react-minimal-pie-chart';
 import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
   partial: {
     backgroundColor: '#a6d0ef',
     height: '100vh',
@@ -15,34 +25,56 @@ const useStyles = makeStyles({
 });
 
 const testData = [
-  { bgcolor: "#6a1b9a", categoryName: "Groceries", completed: 60.27 },
-  { bgcolor: "#00695c", categoryName: "Rent", completed: 30.65 },
-  { bgcolor: "#ef6c00", categoryName: "Utilities", completed: 53.45 },
+  { bgcolor: "#6a1b9a", category: "Groceries", value: 60.00 },
+  { bgcolor: "#00695c", category: "Rent", value: 30.50 },
+  { bgcolor: "#ef6c00", category: "Utilities", value: 53.45 },
 ];
 
 export default function AccountReport() {
   const classes = useStyles();
 
-  const sectionLabeler = dataEntry => {
-    const percentage = Math.round(dataEntry.percentage) + "%";
-    const value = dataEntry.value;
-    const title = dataEntry.title;
+  const getTotal = data => {
+    let total = 0;
+    data.forEach(item => {
+      total += item.value;
+    });
 
-    return `${percentage}\n$${value}\n${title}`;
+    return total;
   };
 
   return (
     <Container maxWidth="xl" className={classes.partial} >
       <CssBaseline />
       <h1>Accounts Overview</h1>
+      <h3>Total Expenses: ${getTotal(testData)}</h3>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Category</TableCell>
+              <TableCell align="right">Expense</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {testData.map(item => (
+              <TableRow key={item.category}>
+                <TableCell component="th" scope="row">
+                  {item.category}
+                </TableCell>
+                <TableCell align="right">${item.value.toFixed(2)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <PieChart
         data={testData.map(item => ({
-          title: item.categoryName,
-          value: item.completed,
+          title: item.category,
+          value: item.value,
           color: item.bgcolor,
         }))}
         labelStyle={{ fontSize: '5px' }}
-        label={({ dataEntry }) => sectionLabeler(dataEntry)}
+        label={({ dataEntry }) => (Math.round(dataEntry.percentage) + "%")}
         segmentsShift={0.5}
         animate
       />
