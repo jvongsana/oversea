15a55432e47bd79691b0b913c14fb27f64083373
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-26 15:46:48 -0800

    Implement edit and delete buttons for account
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- react-front-end/src/hooks/useApplicationData.js
+++ react-front-end/src/hooks/useApplicationData.js
@@ -1,10 +1,13 @@
-import {useEffect, useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from "axios";
 import reducer, {
   SET_ACCOUNT,
   SET_NEW_ACCOUNT,
   SET_APPLICATION_DATA,
-  SET_CATEGORY
+  SET_CATEGORY,
+  SET_RENAME_ACCOUNT,
+  SET_DELETE_ACCOUNT,
+  SET_TRANSACTIONS
 } from "../reducers/application";
 
 export function useApplicationData() {
@@ -17,10 +20,10 @@ export function useApplicationData() {
   });
 
   const setAccount = account => {
-    dispatch({ 
+    dispatch({
       type: SET_ACCOUNT,
-      ...state, 
-      account 
+      ...state,
+      account
     });
   };
 
@@ -38,40 +41,131 @@ export function useApplicationData() {
         transactions: all[2].data,
         transaction_types: all[3].data
       });
-    })
+    });
   }, []);
 
   const addAccount = (user_id, account) => {
     const url = 'http://localhost:8080/api/accounts';
     axios.post(url, { user_id: user_id, name: account })
-    .then((res) => {
-      dispatch({
-        type: SET_NEW_ACCOUNT,
-        user_id,
-        account
-      });
-    })
-    .catch((err) => console.log("error is ", err));
-  }
+      .then((res) => {
+        dispatch({
+          type: SET_NEW_ACCOUNT,
+          user_id,
+          account
+        });
+      })
+      .catch((err) => console.log("error is ", err));
+  };
 
-  const addCategory = (category) => { 
-    console.log('category', category)
+  const addCategory = (category) => {
+    console.log('category', category);
     let url = 'http://localhost:8080/api/categories';
-    axios.post(url, {name: category})
-    .then((res) => {
-      dispatch({
-        type: SET_CATEGORY,
-        category
+    axios.post(url, { name: category })
+      .then((res) => {
+        dispatch({
+          type: SET_CATEGORY,
+          category
+        });
+      })
+      .catch((err) => console.log("error is ", err));
+  };
+
+  const renameAccount = (accountName, newAccountName) => {
+    const accounts = [...state.accounts];
+    let accountID;
+
+    for (const account of accounts) {
+      if (account.name === accountName) {
+        accountID = account.id;
+        account.name = newAccountName;
+      }
+    }
+
+    const url = `http://localhost:8080/api/accounts/${accountID}`;
+    return axios.put(url, { name: newAccountName })
+      .then(() => {
+        console.log(`Account renamed: ${accountName} => ${newAccountName}`);
+        dispatch({
+          type: SET_RENAME_ACCOUNT,
+          account: newAccountName,
+          accounts
+        });
       });
-    })
-    .catch((err) => console.log("error is ", err));
-   
-  }
+  };
+
+  const deleteAccount = accountName => {
+    const accounts = [...state.accounts];
+    let accountID;
+
+    for (let account of accounts) {
+      if (account.name === accountName) {
+        accountID = account.id;
+        account = null;
+      }
+    }
+
+    const url = `http://localhost:8080/api/accounts/${accountID}`;
+    return axios.delete(url)
+      .then(() => {
+        console.log(`Account deleted: ${accountName}`);
+        dispatch({
+          type: SET_DELETE_ACCOUNT,
+          accounts
+        });
+      });
+  };
+
+  const editTransaction = (id, payee, amount, categoryID, transactionTypeID) => {
+    const transactions = [...state.transactions];
+
+    for (const transaction of transactions) {
+      if (transaction.id === id) {
+        transaction.payee = payee;
+        transaction.amount_cents = amount * 100;
+        transaction.category_id = categoryID;
+        transaction.transaction_type_id = transactionTypeID;
+      }
+    }
+
+    const url = `http://localhost:8080/api/transactions/${id}`;
+    return axios.put(url, { payee, amount, categoryID, transactionTypeID })
+      .then(() => {
+        dispatch({
+          type: SET_TRANSACTIONS,
+          transactions
+        });
+        console.log(`Transaction id: ${id} edited payee to: ${payee}, amount to: ${amount * 100}, category_id to: ${categoryID}, transaction_type_id to: ${transactionTypeID}`);
+      });
+  };
+
+  const deleteTransaction = id => {
+    const transactions = [...state.transactions];
+
+    for (let transaction of transactions) {
+      if (transaction.id === id) {
+        transaction = null;
+      }
+    }
+
+    const url = `http://localhost:8080/api/transactions/${id}`;
+    return axios.put(url)
+      .then(() => {
+        dispatch({
+          type: SET_TRANSACTIONS,
+          transactions
+        });
+        console.log(`Transaction id: ${id} deleted.`);
+      });
+  };
 
   return {
     state,
     setAccount,
     addAccount,
-    addCategory
+    addCategory,
+    renameAccount,
+    deleteAccount,
+    editTransaction,
+    deleteTransaction
   };
 }
--- react-front-end/src/reducers/application.js
+++ react-front-end/src/reducers/application.js
@@ -1,28 +1,41 @@
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_ACCOUNT = "SET_ACCOUNT";
 const SET_CATEGORY = "SET_CATEGORY";
-const SET_TRANSACTION = "SET_TRANSACTION";
+const SET_TRANSACTIONS = "SET_TRANSACTIONS";
 const SET_NEW_ACCOUNT = "SET_NEW_ACCOUNT";
+const SET_RENAME_ACCOUNT = "SET_RENAME_ACCOUNT";
+const SET_DELETE_ACCOUNT = "SET_DELETE_ACCOUNT";
 
-const reducer = function(state, action) {
-  console.log('state', state)
-  console.log('action', action)
+const reducer = function (state, action) {
   switch (action.type) {
     case SET_ACCOUNT:
       return {
         ...state,
         account: action.account
-      }
+      };
     case SET_NEW_ACCOUNT:
-      return { 
-        ...state, 
-        accounts: 
-          [...state.accounts, 
-            { user_id: action.user_id, 
-              name: action.account
-            }
+      return {
+        ...state,
+        accounts:
+          [...state.accounts,
+          {
+            user_id: action.user_id,
+            name: action.account
+          }
           ]
-      }
+      };
+    case SET_RENAME_ACCOUNT:
+      return {
+        ...state,
+        account: action.account,
+        accounts: action.accounts
+      };
+    case SET_DELETE_ACCOUNT:
+      return {
+        ...state,
+        account: "Checking",
+        accounts: action.accounts
+      };
     case SET_APPLICATION_DATA:
       return {
         ...state,
@@ -30,23 +43,37 @@ const reducer = function(state, action) {
         categories: action.categories,
         transaction_types: action.transaction_types,
         transactions: action.transactions
-      }
+      };
     case SET_CATEGORY:
       return {
         ...state,
-        categories: 
-          [...state.categories,
-            { 
+        categories:
+          [
+            ...state.categories,
+            {
               name: action.category
             }
           ]
-      }
+      };
+    case SET_TRANSACTIONS:
+      return {
+        ...state,
+        transactions: action.transactions
+      };
     default:
       throw new Error(
         `Tried to reduce with unsupported action type: ${action.type}`
       );
   }
-} 
+};
 
 export default reducer;
-export { SET_APPLICATION_DATA, SET_ACCOUNT, SET_CATEGORY, SET_TRANSACTION, SET_NEW_ACCOUNT };
+export {
+  SET_APPLICATION_DATA,
+  SET_ACCOUNT,
+  SET_CATEGORY,
+  SET_TRANSACTIONS,
+  SET_NEW_ACCOUNT,
+  SET_RENAME_ACCOUNT,
+  SET_DELETE_ACCOUNT
+};
